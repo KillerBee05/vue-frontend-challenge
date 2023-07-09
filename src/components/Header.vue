5ac0966a64cb4e9730e34396728ee9c6b7092ef4
Evan Herring <86384729+Evan-Herring@users.noreply.github.com>
2023-07-09 11:38:43 -0700

    adding mobile display

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -41,25 +41,24 @@ export default {
 };
 </script>
 
-
 <template>
-  <div class="flex items-center mt-20 mb-12">
-    <div class="mr-4">
+  <div class="flex flex-col items-center mt-20 mb-12 md:flex-row md:items-center">
+    <div class="mb-4 md:mr-5 md:mb-0">
       <div v-if="!athlete.profile_image" class="w-24 h-24 rounded-full flex items-center justify-center" :style="getInitialsBgStyle(athlete.name)">
         <span class="text-white text-3xl font-bold">{{ getInitials(athlete.name) }}</span>
       </div>
       <img v-else class="w-24 h-24 rounded-full mt-1" :src="athlete.profile_image" alt="Avatar" />
     </div>
-    <div class="flex flex-col">
-      <div class="flex items-center">
-        <h2 v-if="!editingName" class="mb-1 mt-1 text-xl font-bold text-blue-500">{{ athlete.name }}</h2>
-        <input v-else class="mb-1 mt-1 text-xl font-bold text-blue-500" type="text" v-model="editedName" />
-        <button class="ml-4 bg-blue-500 text-white px-2 py-1 rounded-md text-sm" @click="toggleNameEditing">
+    <div class="flex flex-col items-center md:items-start">
+      <div class="flex items-start mb-4 md:mb-0">
+        <h2 v-if="!editingName" class="mb-1 mt-1 text-xl font-bold text-blue-500 text-left">{{ athlete.name }}</h2> 
+        <input v-else class="mb-1 mt-1 text-xl font-bold text-blue-500 text-left" type="text" v-model="editedName" /> 
+        <button class="ml-4 bg-blue-500 text-white px-2 py-1 mt-1 rounded-md text-sm" @click="toggleNameEditing">
           {{ editingName ? 'Save' : 'Edit' }}
         </button>
       </div>
-      <div class="flex">
-        <ul class="mr-10">
+      <div class="flex flex-col md:flex-row md:items-start"> 
+        <ul class="mb-4 md:mr-10 md:mb-0">
           <li>
             <label class="font-bold">Sport:</label>
             {{ athlete.sport }}
@@ -73,7 +72,7 @@ export default {
             {{ athlete.club.name }}
           </li>
         </ul>
-        <ul class="ml-8">
+        <ul>
           <li>
             <label class="font-bold">High School:</label>
             {{ athlete.high_school.name }}
@@ -89,8 +88,9 @@ export default {
         </ul>
       </div>
     </div>
-    <div class="flex items-center ml-auto">
-      <img class="w-10 h-10" src="logo-placeholder.png" alt="Logo" />
+    <div class="md:ml-auto">
+        <img class="w-10 h-10" src="logo-placeholder.png" alt="Logo" />
     </div>
+
   </div>
 </template>
