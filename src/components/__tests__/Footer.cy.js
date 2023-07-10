4720dccddc9e718dab706921ab4bc2d3cda0f238
Evan Herring <86384729+Evan-Herring@users.noreply.github.com>
2023-07-10 08:41:36 -0700

    Updating footer test

--- src/components/__tests__/Footer.cy.js
+++ src/components/__tests__/Footer.cy.js
@@ -1,14 +1,22 @@
-import { mount } from '@vue/test-utils';
 import Footer from '@/components/Footer.vue';
+import { mount } from '@cypress/vue';
 
 describe('Footer Component', () => {
   it('displays the footer text correctly', () => {
-    const wrapper = mount(Footer);
+    mount(Footer);
 
-    expect(wrapper.find('footer').exists()).to.be.true;
-    expect(wrapper.text()).to.contain('*Rankings for Division I schools based on NCAA data (www.ncaa.com) and rankings for Division II & III schools are based on data from Hero Sports (www.herosports.com/rankings)');
-    expect(wrapper.text()).to.contain('** GPA is based on SportsRecruits members who have shown interest in (favorited) the school and have provided their GPA on their profile');
-    expect(wrapper.text()).to.contain('***SAT and ACT scores based on national data provided by the National Center of Education Statistics-');
-    expect(wrapper.text()).to.contain('https://nces.ed.gov/ipeds/');
+    cy.get('footer').should('be.visible');
+    cy.get('footer p').should(
+      'contain',
+      '*Rankings for Division I schools based on NCAA data (www.ncaa.com) and rankings for Division II & III schools are based on data from Hero Sports (www.herosports.com/rankings)',
+    );
+    cy.get('footer p').should(
+      'contain',
+      '** GPA is based on SportsRecruits members who have shown interest in (favorited) the school and have provided their GPA on their profile',
+    );
+    cy.get('footer p').should(
+      'contain',
+      '***SAT and ACT scores based on national data provided by the National Center of Education Statistics- https://nces.ed.gov/ipeds/',
+    );
   });
 });
