8e0ebc3928ac0494a1eb9aa1eceb16f778a03b1e
Evan Herring <86384729+Evan-Herring@users.noreply.github.com>
2023-07-10 15:46:35 -0700

    Fixing footer styles

--- src/components/AcademicFitReport.vue
+++ src/components/AcademicFitReport.vue
@@ -17,13 +17,13 @@ export default {
 
 
 <template>
-  <div>
-    <div class="py-2 px-4 left-0 right-0 z-10" style="background-color: #00B4FF;"></div>
-    <div class="px-4 sm:px-8 md:px-6">
+  <div class="flex flex-col min-h-screen">
+    <div class="py-2 px-4 left-0 right-0 z-10" style="background-color: #00B4FF;" />
+    <div class="flex-grow px-4 sm:px-8 md:px-6">
       <Header :athlete="athlete" />
       <Table :playerGpa="athlete.gpa" :report="athlete.report" />
-      <Footer />
     </div>
-    <div class="py-2 px-4 left-0 right-0 z-10 mt-4" style="background-color: #00B4FF;"></div>
+    <Footer />
+    <div class="py-2 px-4 left-0 right-0 z-10 mt-4" style="background-color: #00B4FF;" />
   </div>
 </template>
